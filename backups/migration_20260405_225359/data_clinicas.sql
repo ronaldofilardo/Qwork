48865879d62d229a33f8217808882edaffeca6c1
ronaldofilardo <ronaldofilardo@gmail.com>
2026-04-26 22:08:44 -0300

    chore: remover arquivos de backup e patches do versionamento
    - Excluir diretorio backups/ (dumps, CSVs, SQLs de sincronizacao)
    - Remover patch files temporarios (all_stash.patch, stash_diff*.patch)
    - Atualizar .gitignore para excluir futuras mudancas

--- backups/migration_20260405_225359/data_clinicas.sql
+++
@@ -1,3 +0,0 @@
-ERROR:  column "entidade_id" does not exist
-LINHA 9:     contrato_aceito, tipo, entidade_id,
-                                    ^
--- backups/migration_20260405_225708/data_audit_auditoria_recibos.sql
+++
@@ -1,4 +0,0 @@
-ERROR:  column "criado_em " does not exist
-LINHA 1: COPY (SELECT * FROM "auditoria_recibos" WHERE "criado_em " >...
-                                                       ^
-DICA:  Perhaps you meant to reference the column "auditoria_recibos.criado_em".
